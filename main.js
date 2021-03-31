bd4650190b6397377a761f5b3c0dfd6528e61995
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-03-31 11:01:10 -0400

    Created id's for hourly input fields and corresponding variables.

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ document.getElementById("currentDay").innerHTML = currentDay.format('dddd MMMM D
 
 let currentHour = parseInt(moment().format('HH'));
 console.log(currentHour);
+
+let input9AM = $("9AM");
+let input10AM = $("10AM");
+let input11AM = $("11AM");
+let input12PM = $("12PM");
+let input1PM = $("1PM");
+let input2PM = $("2PM");
+let input3PM = $("3PM");
+let input4PM = $("4PM");
+let input5PM = $("5PM");
