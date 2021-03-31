288da0a8b5546356c2cd6ee5236763cca866dcf5
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-03-31 10:48:45 -0400

    Created variable that tracks current hour.

--- main.js
+++ main.js
@@ -2,11 +2,8 @@ console.log("JS Connected");
 
 let currentDay = moment();
 
-// moment(currentDay).format('MM/DD/YYYY');
-// $(document).ready(function() {
-//     var todayDate = moment().format('dddd, MMM Do YYYY');
-//     currentDate.html(todayDate);
-
-// })
 console.log(currentDay.format('dddd MMMM Do YYYY'));
 document.getElementById("currentDay").innerHTML = currentDay.format('dddd MMMM Do YYYY');
+
+let currentHour = parseInt(moment().format('HH'));
+console.log(currentHour);
