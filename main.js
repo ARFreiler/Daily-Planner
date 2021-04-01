09c5f3e79b4adfda96774e80a1ac81b360c3c348
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-04-01 19:17:19 -0400

    Created function to get user inout from local storage and set to daily planner app.

--- main.js
+++ main.js
@@ -23,63 +23,42 @@ for (let i = 0; i < hours.length; i++) {
 }
 
 // Variables for Interfacing with Planner
-let input9AM = $("#9AM");
-let input10AM = $("#10AM");
-let input11AM = $("#11AM");
-let input12PM = $("#12PM");
-let input1PM = $("#1PM");
-let input2PM = $("#2PM");
-let input3PM = $("#3PM");
-let input4PM = $("#4PM");
-let input5PM = $("#5PM");
+let input9AM = $("#hour9");
+let input10AM = $("#hour10");
+let input11AM = $("#hour11");
+let input12PM = $("#hour12");
+let input1PM = $("#hour13");
+let input2PM = $("#hour14");
+let input3PM = $("#hour15");
+let input4PM = $("#hour16");
+let input5PM = $("#hour17");
 
 // Event Listener that sets User Input to Local Storage
 $("button").on("click", function() {
-    localStorage.setItem("9AM", (input9AM.val()));
-    localStorage.setItem("10AM", (input10AM.val()))
-    localStorage.setItem("11AM", (input11AM.val()))
-    localStorage.setItem("12PM", (input12PM.val()))
-    localStorage.setItem("1PM", (input1PM.val()))
-    localStorage.setItem("2PM", (input2PM.val()))
-    localStorage.setItem("3PM", (input3PM.val()))
-    localStorage.setItem("4PM", (input4PM.val()))
-    localStorage.setItem("5PM", (input5PM.val()))
+    localStorage.setItem("9", (input9AM.val()));
+    localStorage.setItem("10", (input10AM.val()))
+    localStorage.setItem("11", (input11AM.val()))
+    localStorage.setItem("12", (input12PM.val()))
+    localStorage.setItem("13", (input1PM.val()))
+    localStorage.setItem("14", (input2PM.val()))
+    localStorage.setItem("15", (input3PM.val()))
+    localStorage.setItem("16", (input4PM.val()))
+    localStorage.setItem("17", (input5PM.val()))
 });
 
-window.onload = function() {
-    var input9AM = localStorage.getItem("9AM");
-    if (input9AM !== null) $('#9AM').val(input9AM);
-    var input10AM = localStorage.getItem("10AM");
-    if (input10AM !== null) $('#10AM').val(input10AM);
-    var input11AM = localStorage.getItem("11AM");
-    if (input11AM !== null) $('#11AM').val(input11AM);
-    var input12PM = localStorage.getItem("12PM");
-    if (input12PM !== null) $('#12PM').val(input12PM);
-    var input1PM = localStorage.getItem("1PM");
-    if (input1PM !== null) $('#1PM').val(input1PM);
-    var input2PM = localStorage.getItem("2PM");
-    if (input2PM !== null) $('#2PM').val(input2PM);
-    var input3PM = localStorage.getItem("3PM");
-    if (input3PM !== null) $('#3PM').val(input3PM);
-    var input4PM = localStorage.getItem("4PM");
-    if (input4PM !== null) $('#4PM').val(input4PM);
-    var input5PM = localStorage.getItem("5PM");
-    if (input5PM !== null) $('#5PM').val(input5PM);
+// Function to Get User Input from Local Storage
+function setData() {
+    console.log("hello");
+    for (let index = 0; index < hours.length; index++) {
+        const hourNumber = hours[index];
+        var input = localStorage.getItem(hourNumber);
+     if (input !== null) $('#hour' + hourNumber).val(input);
+     console.log("hour" + hourNumber);
+    }
 }
 
-// function setData() {
-//     $("#input9AM").append(localStorage.getItem("9AM"));
-//     $("#input10AM").append(localStorage.getItem("10AM"));
-//     $("input11AM").append(localStorage.getItem("11AM"));
-//     $("input12PM").append(localStorage.getItem("12PM"));
-//     $("input1PM").append(localStorage.getItem("1PM"));
-//     $("input2PM").append(localStorage.getItem("2PM"));
-//     $("input3PM").append(localStorage.getItem("3PM"));
-//     $("input4PM").append(localStorage.getItem("4PM"));
-//     $("input5PM").append(localStorage.getItem("5PM"));
-// }
+setData();
 
-// setData();
 
 
 
