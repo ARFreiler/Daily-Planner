714094d9ad5c50036906cd4cf00ed1e3690eeafa
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-03-31 22:32:28 -0400

    Created function to get user input from local storage and set to input fields.

--- main.js
+++ main.js
@@ -22,19 +22,7 @@ for (let i = 0; i < hours.length; i++) {
     } else $("#" + hour).addClass("future");
 }
 
-// Time Check to Style Background Colors
-function compareHour() {
-    if (hours.indexOf.value < currentHour) {
-        document.getElementsByClassName("col-10").style.backgroundColor = "#d3d3d3";
-    } else if (hours.indexOf.value === currentHour) {
-        document.getElementsByClassName("col-10").style.backgroundColor = "#ff6961";
-    } else document.getElementsByClassName("col-10").style.backgroundColor= "#77dd77"; 
-}
-
-
-
 // Variables for Interfacing with Planner
-// let input9AM = $("col-10");
 let input9AM = $("#9AM");
 let input10AM = $("#10AM");
 let input11AM = $("#11AM");
@@ -45,39 +33,53 @@ let input3PM = $("#3PM");
 let input4PM = $("#4PM");
 let input5PM = $("#5PM");
 
-// Variable for Setting User Input
-// let input = document.getElementsByTagName("input");
-let span = document.getElementsByTagName("span");
-let input = document.getElementById("9AM");
-let text = document.getElementsByClassName("col-10");
-let button = document.getElementById("button");
-
-/* Working-ish */
-// button.addEventListener("click", function() {
-//     let input = document.getElementById("9AM").value;
-//     localStorage.setItem("input", input);
-// });
-
+// Event Listener that sets User Input to Local Storage
 $("button").on("click", function() {
     localStorage.setItem("9AM", (input9AM.val()));
     localStorage.setItem("10AM", (input10AM.val()))
     localStorage.setItem("11AM", (input11AM.val()))
-    localStorage.setItem("12AM", (input12PM.val()))
-    localStorage.setItem("13PM", (input1PM.val()))
-    localStorage.setItem("14PM", (input2PM.val()))
-    localStorage.setItem("15PM", (input3PM.val()))
-    localStorage.setItem("16PM", (input4PM.val()))
-    localStorage.setItem("17PM", (input5PM.val()))
+    localStorage.setItem("12PM", (input12PM.val()))
+    localStorage.setItem("1PM", (input1PM.val()))
+    localStorage.setItem("2PM", (input2PM.val()))
+    localStorage.setItem("3PM", (input3PM.val()))
+    localStorage.setItem("4PM", (input4PM.val()))
+    localStorage.setItem("5PM", (input5PM.val()))
 });
 
-$("input9AM").append(localStorage.getItem("9AM"));
-$("input10AM").append(localStorage.getItem("10AM"));
-$("input11AM").append(localStorage.getItem("11AM"));
-$("input12PM").append(localStorage.getItem("12PM"));
-$("input1PM").append(localStorage.getItem("1PM"));
-$("input2PM").append(localStorage.getItem("2PM"));
-$("input3PM").append(localStorage.getItem("3PM"));
-$("input4PM").append(localStorage.getItem("4PM"));
-$("input5PM").append(localStorage.getItem("5PM"));
+window.onload = function() {
+    var input9AM = localStorage.getItem("9AM");
+    if (input9AM !== null) $('#9AM').val(input9AM);
+    var input10AM = localStorage.getItem("10AM");
+    if (input10AM !== null) $('#10AM').val(input10AM);
+    var input11AM = localStorage.getItem("11AM");
+    if (input11AM !== null) $('#11AM').val(input11AM);
+    var input12PM = localStorage.getItem("12PM");
+    if (input12PM !== null) $('#12PM').val(input12PM);
+    var input1PM = localStorage.getItem("1PM");
+    if (input1PM !== null) $('#1PM').val(input1PM);
+    var input2PM = localStorage.getItem("2PM");
+    if (input2PM !== null) $('#2PM').val(input2PM);
+    var input3PM = localStorage.getItem("3PM");
+    if (input3PM !== null) $('#3PM').val(input3PM);
+    var input4PM = localStorage.getItem("4PM");
+    if (input4PM !== null) $('#4PM').val(input4PM);
+    var input5PM = localStorage.getItem("5PM");
+    if (input5PM !== null) $('#5PM').val(input5PM);
+}
+
+// function setData() {
+//     $("#input9AM").append(localStorage.getItem("9AM"));
+//     $("#input10AM").append(localStorage.getItem("10AM"));
+//     $("input11AM").append(localStorage.getItem("11AM"));
+//     $("input12PM").append(localStorage.getItem("12PM"));
+//     $("input1PM").append(localStorage.getItem("1PM"));
+//     $("input2PM").append(localStorage.getItem("2PM"));
+//     $("input3PM").append(localStorage.getItem("3PM"));
+//     $("input4PM").append(localStorage.getItem("4PM"));
+//     $("input5PM").append(localStorage.getItem("5PM"));
+// }
+
+// setData();
+
 
 
