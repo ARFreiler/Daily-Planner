305cda8a36062feb73367ab7f9cc1140438e551c
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-03-31 21:32:39 -0400

    Created for loop to track hours and style input fields accordingly.

--- main.js
+++ main.js
@@ -8,53 +8,48 @@ document.getElementById("currentDay").innerHTML = currentDay.format('dddd MMMM D
 
 // Array of Planner Hours 
 let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-
+// console.log(hours);
 // Set Current Hour for User Interface
 let currentHour = parseInt(moment().format('HH'));
 console.log(currentHour);
 
+for (let i = 0; i < hours.length; i++) {
+    const hour = hours[i];
+    if (hour < currentHour) {
+        $("#" + hour).addClass("past");
+    } else if (hour === currentHour) {
+        $("#" + hour).addClass("present");
+    } else $("#" + hour).addClass("future");
+}
+
+// Time Check to Style Background Colors
+function compareHour() {
+    if (hours.indexOf.value < currentHour) {
+        document.getElementsByClassName("col-10").style.backgroundColor = "#d3d3d3";
+    } else if (hours.indexOf.value === currentHour) {
+        document.getElementsByClassName("col-10").style.backgroundColor = "#ff6961";
+    } else document.getElementsByClassName("col-10").style.backgroundColor= "#77dd77"; 
+}
+
+
+
 // Variables for Interfacing with Planner
 // let input9AM = $("col-10");
-let input9AM = $("9AM");
-let input10AM = $("10AM");
-let input11AM = $("11AM");
-let input12PM = $("12PM");
-let input1PM = $("1PM");
-let input2PM = $("2PM");
-let input3PM = $("3PM");
-let input4PM = $("4PM");
-let input5PM = $("5PM");
+let input9AM = $("#9AM");
+let input10AM = $("#10AM");
+let input11AM = $("#11AM");
+let input12PM = $("#12PM");
+let input1PM = $("#1PM");
+let input2PM = $("#2PM");
+let input3PM = $("#3PM");
+let input4PM = $("#4PM");
+let input5PM = $("#5PM");
 
 // Variable for Setting User Input
 // let input = document.getElementsByTagName("input");
 let span = document.getElementsByTagName("span");
 let input = document.getElementById("9AM");
 let text = document.getElementsByClassName("col-10");
-
-// Event Listener for Save Buttons
-// $(".saveBtn").click(function() {
-//     console.log("Functioning");
-//     localStorage.setItem
-// });
-
-// $(".saveBtn").click(function() {
-//     let userInput = input.value;
-//     localStorage.setItem('userInput', userInput);
-//     console.log(userInput);
-// });
-
-// $(".saveBtn").click(function() {
-//     let userInput = document.getElementsByTagName("input").value;
-//     localStorage.setItem('userInput', userInput);
-//     console.log(userInput);
-// });
-
-// function saveInput() {
-//     let userInput = document.getElementsByClassName("col-10").value;
-//     localStorage.setItem('userInput', userInput);
-//     console.log(userInput);
-// };
-
 let button = document.getElementById("button");
 
 /* Working-ish */
@@ -64,7 +59,7 @@ let button = document.getElementById("button");
 // });
 
 $("button").on("click", function() {
-    localStorage.setItem("9AM", (input9AM.val()))
+    localStorage.setItem("9AM", (input9AM.val()));
     localStorage.setItem("10AM", (input10AM.val()))
     localStorage.setItem("11AM", (input11AM.val()))
     localStorage.setItem("12AM", (input12PM.val()))
@@ -76,3 +71,13 @@ $("button").on("click", function() {
 });
 
 $("input9AM").append(localStorage.getItem("9AM"));
+$("input10AM").append(localStorage.getItem("10AM"));
+$("input11AM").append(localStorage.getItem("11AM"));
+$("input12PM").append(localStorage.getItem("12PM"));
+$("input1PM").append(localStorage.getItem("1PM"));
+$("input2PM").append(localStorage.getItem("2PM"));
+$("input3PM").append(localStorage.getItem("3PM"));
+$("input4PM").append(localStorage.getItem("4PM"));
+$("input5PM").append(localStorage.getItem("5PM"));
+
+
